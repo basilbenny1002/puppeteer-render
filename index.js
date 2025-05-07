d5f7c48e25c00f396b33bbdc1838fab496b0589e
Ank <ankon495@gmail.com>
2025-05-07 14:43:03 +0600

    added prints

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.get("/scrape", (req, res) => {
+  console.log("Scraping...");
   scrapeLogic(res);
 });
 
 app.get("/", (req, res) => {
+  console.log("root render pputter server");
   res.send("Render Puppeteer server is up and running!");
 });
 
