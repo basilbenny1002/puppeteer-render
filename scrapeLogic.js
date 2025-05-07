93fb1c1ebf08f6c508978edf3d9c1f775fca9f4b
Ank <ankon495@gmail.com>
2025-05-07 14:45:58 +0600

    update launch

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -3,7 +3,18 @@ require("dotenv").config();
 
 const scrapeLogic = async (res) => {
   // Launch the browser and open a new blank page
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
   const page = await browser.newPage();
 
   // Navigate the page to a URL
